a0577f77a4aec36577165a6138a4225c27b8cab2
Bran7astic <budhanbrandon04@gmail.com>
2026-04-19 10:24:55 -0400

    refactor: improve NavBar layout for flexibility

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,40 +7,40 @@ import Typography from "@mui/material/Typography";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import ShoppingCart from "@mui/icons-material/ShoppingCart";
 import { useNavigate } from "react-router-dom";
-import pawmodoroIcon from '/pawmodoro.png'
-import Box from "@mui/material/Box";
+import pawmodoroIcon from "/pawmodoro.png";
 
 export default function NavBar() {
-    
-    const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	return (
 		<AppBar>
-			<Toolbar sx={{justifyContent: "center", height: 75}}>
-				<Stack direction="row" width="100%" sx={{justifyContent: "center", alignItems: "center"}}>
-					<Stack
-						direction="row"
-                        sx = {{
-                            alignItems: "center",
-                            justifyContent: "center"
-                        }}
-						spacing={20}
-					>
-						<IconButton aria-label="Profile" sx={{ color: "#FFFFFF" }} onClick={() => navigate('/shop')}>
-							<ShoppingCart />
-						</IconButton>
+			<Toolbar
+				sx={{
+					height: 75,
+					px: 2,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-evenly",
+				}}
+			>
+				<IconButton aria-label="Shop" sx={{ color: "#FFFFFF" }} onClick={() => navigate("/shop")}>
+					<ShoppingCart />
+				</IconButton>
 
-
-                        <Stack direction="row" spacing={1} sx={{justifyContent: "center", alignItems: "center"}}>
-                            <Box component="img" src={pawmodoroIcon} sx={{width: "3em"}}/>
-						    <Typography variant="h4" sx={{cursor: "pointer"}} onClick={() => navigate('/')}>Pawmodoro</Typography>
-                        </Stack>
-
-						<IconButton aria-label="Profile" sx={{ color: "#FFFFFF" }} onClick={() => navigate('/profile')}>
-							<AccountCircle />
-						</IconButton>
-					</Stack>
+				<Stack direction="row" spacing={1} sx={{ justifyContent: "center", alignItems: "center" }}>
+					<Box component="img" src={pawmodoroIcon} sx={{ width: "3em" }} />
+					<Typography variant="h4" sx={{ cursor: "pointer" }} onClick={() => navigate("/")}>
+						Pawmodoro
+					</Typography>
 				</Stack>
+
+				<IconButton
+					aria-label="Profile"
+					sx={{ color: "#FFFFFF" }}
+					onClick={() => navigate("/profile")}
+				>
+					<AccountCircle />
+				</IconButton>
 			</Toolbar>
 		</AppBar>
 	);
