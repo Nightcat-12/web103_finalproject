76f83f7bf3b62e75e8e2c20da9f49d4bfc38c3af
dganesh05 <dganesh3105@gmail.com>
2026-04-25 17:43:08 -0400

    feat: add rate limiting to study session routes

--- server/routes/studySessions.js
+++ server/routes/studySessions.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import rateLimit from 'express-rate-limit'
 import SessionsController from '../controllers/studySessions.js'
 
 const router = express.Router()
 
+const studySessionsLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 200, // limit each IP to 200 study session requests per window
+})
+
 // == Study Sessions ==
-router.get('/:uid', SessionsController.getAllSessionsFromUser)
-router.post('/', SessionsController.addSession)
+router.get('/:uid', studySessionsLimiter, SessionsController.getAllSessionsFromUser)
+router.post('/', studySessionsLimiter, SessionsController.addSession)
 
 export default router
